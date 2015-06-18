29fa0ae0fda77548ee73139b72db5c52727cf73a
Michael Spector <michael.l.spector@gmail.com>
2015-06-18 18:08:15 -0400

    cleanup

---
+++ src/main.py
@@ -0,0 +1,41 @@
+"""
+=============================================
+~~Starcraft Assistive AI~~
+
+The Starcraft Assistive AI encodes Starcraft II knowledge and uses it to generate predictions.
+
+Press the "Print Screen" key while in-game to capture the screen for processing.
+Press "Escape" to quit.
+
+**You must be in full-screen windowed mode!**
+=============================================
+"""
+print(__doc__)
+
+from sc2centaur import *
+import keyboardhook
+
+def main(): 
+    training_directory="/home/michael/projects/sc2centaur/data/training_data/"
+    observation = ['6:58', '[13, 0, 0, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]']
+    key = None
+
+    hook = keyboardhook.GlobalInput()
+    sc2c = SC2C(training_directory)
+
+    while key != 'Snapshot':
+        key = hook.getKey()
+
+    print('success')
+    nexus_identified = sc2c.find_buildings('observations\\observation.bmp')
+    #time = sc2c.get_game_time()
+    time = "<insert time here>"
+    if nexus_identified:
+        print("Enemy expansion identified at T="+time)
+    else:
+        print("No expansion detected.")
+    #test_replay = '/home/michael/projects/sc2centaur/data/replays/4gate_1.SC2Replay'
+    #sc2c.test_plugins(test_replay)
+
+if __name__ == '__main__':
+    main()
---
+++ src/sc2helper.py
@@ -0,0 +1,63 @@
+"""
+==================================
+Starcraft Assistive AI: Helper functions
+==================================
+"""
+import csv
+import datetime
+import math
+import operator
+#import ipdb
+def time_string_to_decimals(hhmmss):
+    [minutes, seconds] = [int(x) for x in hhmmss.split(':')]
+    time = datetime.timedelta(minutes=minutes, seconds=seconds)
+    return time
+
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x]-instance2[x]),2)
+    return math.sqrt(distance) 
+
+def csv_read(files):
+    training_data =[]
+    for filename in files:
+        vectors = []
+        with open(filename, 'rb') as csvfile:
+            data=[]
+            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in spamreader:
+                row[0]=time_string_to_decimals(row[0])
+                data.append(row)
+            vectors.append(data)
+        training_data.append(vectors)
+    return training_data
+
+
+
+
+
+
+
+def getNeighbors(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance)-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response]+=1
+        else:
+            classVotes[response]=1
+    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
